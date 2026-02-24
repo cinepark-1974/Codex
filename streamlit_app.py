ae50036396db014d15bbc59982e2824e28f67767
cinepark-1974 <138092407+cinepark-1974@users.noreply.github.com>
2026-02-24 22:05:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
-app
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio            # Plotly 차트를 이미지로 변환하기 위해 필요
+import OpenAI as Chatgpt4
+from PyPDF2 import PdfReader
+import json
+import io
+import re
+
+st.title("블루진픽처스 시나리오 닥터")
+st.write("BLUE JEANS PICTURES SCRIPT DOCTOR 1.0")
